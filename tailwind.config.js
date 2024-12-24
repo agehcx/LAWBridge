d9f85c22f149647212f53bd2a0989fa0848d9e67
agehcx <chega.malee@gmail.com>
2024-12-25 00:42:21 +0700

    fix: fix deploy (?)

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,16 @@ module.exports = {
   content: [
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
-      colors: {
-        background: "#0F082C",
-        "accent-purple": "#D1C4E9",
-        "accent-purple-foreground": "#651FFF",
-      },
+      // colors: {
+      //   background: "#0F082C",
+      //   "accent-purple": "#D1C4E9",
+      //   "accent-purple-foreground": "#651FFF",
+      // },
     },
   },
   plugins: [],
